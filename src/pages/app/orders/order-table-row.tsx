cf374a3ffc6d0ea96924dd483ea0200c135e2ded
devJonasfranco <minhacontajonas@gmail.com>
2024-08-28 15:30:25 -0300

    implementado no dashboard grafico de receitas

--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -1,49 +1,49 @@
-import { ArrowRight, Search, X } from 'lucide-react'
+import { ArrowRight, Search, X } from "lucide-react";
 
-import { Button } from '@/components/ui/button'
-import { Dialog, DialogTrigger } from '@/components/ui/dialog'
-import { TableCell, TableRow } from '@/components/ui/table'
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
+import { TableCell, TableRow } from "@/components/ui/table";
 
-import { Orderdetails } from './order-details'
+import { Orderdetails } from "./order-details";
 
 export function OrderTableRow() {
-  return (
-    <TableRow>
-      <TableCell>
-        <Dialog>
-          <DialogTrigger asChild>
-            <Button variant="outline" size="xs">
-              <Search className="h-3 w-3" />
-              <span className="sr-only">Detalhes do pedido</span>
-            </Button>
-          </DialogTrigger>
-          <Orderdetails />
-        </Dialog>
-      </TableCell>
-      <TableCell className="font-mono text-xs font-medium">
-        adf1a65d1fa6df
-      </TableCell>
-      <TableCell className="text-muted-foreground">há 15 minutos</TableCell>
-      <TableCell>
-        <div className="flex items-center gap-2">
-          <span className="h-2 w-2 rounded-full bg-slate-400"></span>
-          <span className="font-medium text-muted-foreground">Pendente</span>
-        </div>
-      </TableCell>
-      <TableCell className="font-medium">Jonas Franco</TableCell>
-      <TableCell className="font-medium">R$ 42,00</TableCell>
-      <TableCell>
-        <Button variant="outline" size="xs">
-          <ArrowRight className="mr-2 h-3 w-3" />
-          Aprovar
-        </Button>
-      </TableCell>
-      <TableCell>
-        <Button variant="ghost" size="xs">
-          <X className="mr-2 h-3 w-3" />
-          Cancelar
-        </Button>
-      </TableCell>
-    </TableRow>
-  )
+	return (
+		<TableRow>
+			<TableCell>
+				<Dialog>
+					<DialogTrigger asChild>
+						<Button variant="outline" size="xs">
+							<Search className="h-3 w-3" />
+							<span className="sr-only">Detalhes do pedido</span>
+						</Button>
+					</DialogTrigger>
+					<Orderdetails />
+				</Dialog>
+			</TableCell>
+			<TableCell className="font-mono text-xs font-medium">
+				adf1a65d1fa6df
+			</TableCell>
+			<TableCell className="text-muted-foreground">há 15 minutos</TableCell>
+			<TableCell>
+				<div className="flex items-center gap-2">
+					<span className="h-2 w-2 rounded-full bg-slate-400" />
+					<span className="font-medium text-muted-foreground">Pendente</span>
+				</div>
+			</TableCell>
+			<TableCell className="font-medium">Jonas Franco</TableCell>
+			<TableCell className="font-medium">R$ 42,00</TableCell>
+			<TableCell>
+				<Button variant="outline" size="xs">
+					<ArrowRight className="mr-2 h-3 w-3" />
+					Aprovar
+				</Button>
+			</TableCell>
+			<TableCell>
+				<Button variant="ghost" size="xs">
+					<X className="mr-2 h-3 w-3" />
+					Cancelar
+				</Button>
+			</TableCell>
+		</TableRow>
+	);
 }
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,31 +1,32 @@
-import { createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter } from "react-router-dom";
 
-import { ErrorPage } from './error-page'
-import { AppLayout } from './pages/_layouts/app'
-import { AuthLayout } from './pages/_layouts/auth'
-import { Dashboard } from './pages/app/dashboard/dashboard'
-import { Orders } from './pages/app/orders/orders'
-import { SignIn } from './pages/auth/sign-in'
-import { SignUp } from './pages/auth/sign-up'
+import { ErrorPage } from "./error-page";
+import { AppLayout } from "./pages/_layouts/app";
+import { AuthLayout } from "./pages/_layouts/auth";
+import { Dashboard } from "./pages/app/dashboard/dashboard";
+import { Orders } from "./pages/app/orders/orders";
+import { SignIn } from "./pages/auth/sign-in";
+import { SignUp } from "./pages/auth/sign-up";
+import { NotFound } from "./pages/404";
 
 export const rounter = createBrowserRouter([
-  {
-    // rota para home
-    path: '/',
-    element: <AppLayout />,
-    errorElement: <ErrorPage />,
-    children: [
-      { path: '/', element: <Dashboard /> },
-      { path: '/orders', element: <Orders /> },
-    ],
-  },
-  {
-    // rota para login
-    path: '/',
-    element: <AuthLayout />,
-    children: [
-      { path: '/sign-in', element: <SignIn /> },
-      { path: '/sign-up', element: <SignUp /> },
-    ],
-  },
-])
+	{
+		// rota para home
+		path: "/",
+		element: <AppLayout />,
+		errorElement: <NotFound />,
+		children: [
+			{ path: "/", element: <Dashboard /> },
+			{ path: "/orders", element: <Orders /> },
+		],
+	},
+	{
+		// rota para login
+		path: "/",
+		element: <AuthLayout />,
+		children: [
+			{ path: "/sign-in", element: <SignIn /> },
+			{ path: "/sign-up", element: <SignUp /> },
+		],
+	},
+]);
